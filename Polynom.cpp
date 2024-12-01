185f0c3933d2668895e984b9bb330b5e3c613424
Kirill Radkevich <Ughlympics>
2024-12-01 14:56:37 +0200

    ADD and MUL

--- Polynom.cpp
+++ Polynom.cpp
@@ -29,12 +29,16 @@ Polynom::Polynom(const std::string& binaryString) {
     }
 }
 
+Polynom::Polynom(const std::bitset<_POWER>& bitset_coeffs) {
+    coefficients = bitset_coeffs; 
+}
+
 Polynom::~Polynom() {}
 
 //output
 std::string Polynom::toBitString() const {
     std::string bitString;
-    for (int i = _POWER; i >= 0; --i) {
+    for (int i = _POWER - 1; i >= 0; --i) {
         bitString += coefficients[i] ? '1' : '0';
     }
     return bitString;
@@ -52,3 +56,44 @@ Polynom Polynom::operator+(const Polynom& pol) const {
     return res;
 }
 
+Polynom Polynom::operator*(const Polynom& pol) const {
+    std::bitset<_DOBLE_POWER> res;
+
+    std::bitset<_DOBLE_POWER> l = double_pol(this->coefficients);
+    //std::bitset<_DOBLE_POWER> r = double_pol(pol.coefficients);
+
+    for (int i = 0; i <= _POWER- 1; ++i, l <<= 1) {
+        if (pol.coefficients[i]) {
+            res ^= l; 
+        }
+    }
+
+    reduction(res);
+    return Polynom(return_size(res));
+}
+
+std::bitset<Polynom::_DOBLE_POWER> Polynom::double_pol(const std::bitset<_POWER>& pol) {
+    std::bitset<Polynom::_DOBLE_POWER> doubled;
+    for (int i = 0; i < _POWER; ++i)
+        doubled[i] = pol[i];
+
+    return doubled;
+}
+
+std::bitset<Polynom::_POWER> Polynom::return_size(const std::bitset<_DOBLE_POWER>& pol) {
+    std::bitset<Polynom::_POWER> undoubled;
+    for (int i = 0; i < _POWER; ++i)
+        undoubled[i] = pol[i];
+
+    return undoubled;
+}
+
+void Polynom::reduction(std::bitset<_DOBLE_POWER>& pol) {
+    std::bitset<_DOBLE_POWER> p;
+    p[_POWER] = p[28] = p[3] = p[1] = p[0] = 1;
+
+    for (int i = _DOBLE_POWER - 1; i >= _POWER; --i) {
+        if (pol[i])
+            pol ^= (p << i - _POWER);
+    }
+}
--- Polynom.h
+++ Polynom.h
@@ -9,13 +9,15 @@
 class Polynom {
 private:
 	static const int _POWER = 443;
-	std::bitset<_POWER + 1> coefficients;
+	static const int _DOBLE_POWER = 2 * _POWER;
+	std::bitset<_POWER> coefficients;
 public:
 
 	//Constr
 	Polynom();
 	Polynom(unsigned _default_num);
 	Polynom(const std::string& binaryString);
+	Polynom(const std::bitset<_POWER>& bitset_coeffs);
 	~Polynom();
 
     std::string toBitString() const;
@@ -23,6 +25,11 @@ public:
 	//overloaded operators
 	Polynom& operator=(const Polynom&);
 	Polynom operator+(const Polynom&) const;
+	Polynom operator*(const Polynom&) const;
 
+    //
+	static std::bitset<_DOBLE_POWER> double_pol(const std::bitset<_POWER >&);
+	static std::bitset<_POWER> return_size(const std::bitset<_DOBLE_POWER>&);
+	static void reduction(std::bitset<_DOBLE_POWER>&);
 };
 
